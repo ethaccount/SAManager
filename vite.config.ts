2ebffbc6df83c7e092e58087f4a3c015968ddcd2
Johnson Chen <chnejohnson@gmail.com>
2025-03-05 13:12:01 +0800

    fix: add vite-plugin-node-polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		vue(),
+		nodePolyfills(),
 		AutoImport({
 			dts: 'src/auto-import.d.ts',
 			imports: ['vue', 'vue-router', 'pinia'],
