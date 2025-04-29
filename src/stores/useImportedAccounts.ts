3b449ce1c1f921f83ea02fd30b0d07ddde3af3f2
Johnson Chen <chnejohnson@gmail.com>
2025-04-29 11:28:27 +0800

    dev: update imported accounts store

--- src/stores/useImportedAccounts.ts
+++ src/stores/useImportedAccounts.ts
@@ -1,39 +1,73 @@
 import { defineStore, storeToRefs } from 'pinia'
+import { EntryPointVersion } from 'sendop'
 
-export type ImportedAccount = {
-	address: string
-	chainId: bigint
-	validationTypes: ValidationType[]
-	accountId: string
-	type: 'SA' | '7702'
-	isDeployed: boolean
-	isModular: boolean
-	supportedEps: string[]
+export enum AccountId {
+	'kernel.advanced.v0.3.1',
+	'biconomy.nexus.1.0.2',
+	'rhinestone.safe7579.v1.0.0',
+	'infinitism.Simple7702Account.0.8.0',
+	'infinitism.SimpleAccount.0.8.0',
 }
 
-export type ValidationType = {
+export const SUPPORTED_ACCOUNTS: Record<
+	AccountId,
+	{
+		supportedEps: EntryPointVersion[]
+		isModular: boolean
+	}
+> = {
+	[AccountId['kernel.advanced.v0.3.1']]: {
+		supportedEps: ['v0.7'],
+		isModular: true,
+	},
+	[AccountId['biconomy.nexus.1.0.2']]: {
+		supportedEps: ['v0.7'],
+		isModular: true,
+	},
+	[AccountId['rhinestone.safe7579.v1.0.0']]: {
+		supportedEps: ['v0.7'],
+		isModular: true,
+	},
+	[AccountId['infinitism.SimpleAccount.0.8.0']]: {
+		supportedEps: ['v0.8'],
+		isModular: false,
+	},
+	[AccountId['infinitism.Simple7702Account.0.8.0']]: {
+		supportedEps: ['v0.8'],
+		isModular: false,
+	},
+}
+
+export type ImportedAccount = {
+	type: 'Smart Account' | 'Smart EOA'
+	address: string
+	chainId: bigint
+	vOptions: ValidationOption[]
+	accountId: AccountId
+	initCode: string | null
+}
+
+export type ValidationOption = {
 	type: 'EOA' | 'Passkey'
 	publicKey: string
 }
 
 export const useImportedAccountsStore = defineStore('useImportedAccountsStore', () => {
-	const importedAccounts = ref<ImportedAccount[]>([
+	const accounts = ref<ImportedAccount[]>([
 		{
+			type: 'Smart Account',
 			address: '0x0000000000000000000000000000000000000000',
 			chainId: 11155111n,
-			validationTypes: [],
-			accountId: '1',
-			type: 'SA',
-			isDeployed: true,
-			isModular: true,
-			supportedEps: [],
+			vOptions: [],
+			accountId: AccountId['kernel.advanced.v0.3.1'],
+			initCode: null,
 		},
 	])
-	const hasImportedAccounts = computed(() => importedAccounts.value.length > 0)
+	const hasAccounts = computed(() => accounts.value.length > 0)
 
 	return {
-		importedAccounts,
-		hasImportedAccounts,
+		accounts,
+		hasAccounts,
 	}
 })
 
