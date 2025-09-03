dae2271d801dce5e3334acfca942ff108c7afd12
Johnson Chen <chnejohnson@gmail.com>
2025-09-03 10:07:37 +0800

    refactor: connect feature

--- src/features/connect/WalletSendCalls.vue
+++ src/features/connect/WalletSendCalls.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
 import { TransactionStatus, useTxModal } from '@/stores/useTxModal'
+import { standardErrors, WalletSendCallsRequest } from '@samanager/sdk'
+import { PendingRequest } from './types'
+
+const props = defineProps<{
+	pendingRequest: PendingRequest<WalletSendCallsRequest['params']>
+}>()
 
 const { status } = useTxModal()
 
@@ -7,10 +13,31 @@ onMounted(() => {
 	// set status to initial like useTxModalStore.openModal()
 	status.value = TransactionStatus.Initial
 })
+
+const executions = computed(() => {
+	const params = props.pendingRequest.params
+	return params[0].calls.map(call => {
+		return {
+			to: call.to ?? '',
+			data: call.data ?? '',
+			value: BigInt(call.value ?? 0n),
+		}
+	})
+})
+
+function onClickTxClose() {
+	props.pendingRequest.reject(standardErrors.provider.userRejectedRequest())
+}
+
+function handleTxSent(hash: string) {
+	props.pendingRequest.resolve({
+		id: hash,
+	})
+}
 </script>
 
 <template>
-	<TxUI :use-modal-specific-style="false" v-bind="$attrs" v-on="$attrs" />
+	<TxUI :executions="executions" :use-modal-specific-style="false" @close="onClickTxClose" @sent="handleTxSent" />
 </template>
 
 <style lang="css" scoped></style>
--- src/features/connect/index.ts
+++ src/features/connect/index.ts
@@ -1,2 +1,3 @@
 export * from './wallet_getCallsStatus'
 export * from './wallet_getCapabilities'
+export * from './types'
